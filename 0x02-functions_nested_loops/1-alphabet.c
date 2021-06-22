af1d39c88d3e1d4dc4f2148fa426a5e2541189d5
louismaximo <louismaxime95@gmail.com>
2021-06-22 13:26:47 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  * print_alphabet - printing
+  * Return - nothing
+  */
+void print_alphabet(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
+
