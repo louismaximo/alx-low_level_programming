ad2a45d16efcbba6d70deb34db6a01b3cebe9977
louismaximo <louismaxime95@gmail.com>
2021-06-23 06:55:33 -0700

    committed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_last_digit -entry
+ *
+ * @n: an integer argument
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+int m = n % 10;
+if (m >= 0)
+{
+_putchar(m + '0');
+return (m);
+}
+_putchar(-m + '0');
+return (-m);
+}
+
