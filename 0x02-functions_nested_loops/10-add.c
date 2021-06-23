d564ded0794ed64a769d098808be110404c78940
louismaximo <louismaxime95@gmail.com>
2021-06-23 07:11:28 -0700

    Yes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns to the result
+ * @a: an integer to add to b
+ * @b: an integer to add to a
+ * Return: void.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
