d482ea0a06a5f782ca5aa1579b5cfb0bcbd7ec69
louismaximo <louismaxime95@gmail.com>
2021-06-24 12:00:57 -0700

    ALX

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print large prime factor
+ * Return: void
+ */
+int main(void)
+{
+long i;
+long num = 612852475143;
+for (i = 2; i < num; i++)
+{
+if (num % i == 0)
+num /= i;
+}
+printf("%li\n", num);
+return (0);
+}
+
